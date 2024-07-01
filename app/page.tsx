5c557e71c4ceb93f42255173e083fc74a95d8c44
Hajin Choi <j8747j@gmail.com>
2024-07-01 21:32:30 +0900

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,134 @@
 import Image from "next/image";
 
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="h-screen flex items-center justify-center bg-pink-200 p-5 space-x-10">
+      <div className="flex-1 w-1/3 h-5/6 items-center justify-center bg-orange-400 ml-10 space-y-10">
+        <div className="text-4xl font-bold pt-10 pb-9 ml-5">
+          Wearter
+        </div>
+        <div className="items-center justify-between flex w-96 h-28 border-solid border-2 border-b-8 border-black rounded-2xl ml-5 mb-4">
+          <div className="w-40 pl-5 items-center justify-center">
+            <div className="text-xl font-extrabold">Casius</div>
+            <div className="font-extrabold text-xs"> Mars, 12AM</div>
+          </div>
+          <div className="w-60 h-full items-center justify-center flex pt-6">
+            <div className="w-24 h-20 ">구름그림</div>
+            <div className="w-28 h-20 text-3xl font-extrabold">85℃</div>
+          </div>
+        </div>
+        <div className="items-center justify-between flex w-96 h-28 border-solid border-2 border-b-8 border-black rounded-2xl ml-5 mb-4 bg-yellow-400">
+          <div className="w-40 pl-5 items-center justify-center">
+            <div className="text-xl font-extrabold">Dlacria</div>
+            <div className="font-extrabold text-xs"> Mars, 12AM</div>
+          </div>
+          <div className="w-60 h-full items-center justify-center flex pt-6">
+            <div className="w-24 h-20 ">구름그림</div>
+            <div className="w-28 h-20 text-3xl font-extrabold">85℃</div>
+          </div>
+        </div>
+        <div className="items-center justify-between flex w-96 h-28 border-solid border-2 border-b-8 border-black rounded-2xl ml-5 mb-4 bg-teal-600">
+          <div className="w-40 pl-5 items-center justify-center">
+            <div className="text-xl font-extrabold">NEW YORK</div>
+            <div className="font-extrabold text-xs"> USA, 12AM</div>
+          </div>
+          <div className="w-60 h-full items-center justify-center flex pt-6">
+            <div className="w-24 h-20 ">맑음</div>
+            <div className="w-28 h-20 text-3xl font-extrabold">85℃</div>
+          </div>
+        </div>
+        <div className="items-center justify-between flex w-96 h-28 border-solid border-2 border-b-8 border-black rounded-2xl ml-5 mb-4 bg-orange-600 ">
+          <div className="w-40 pl-5 items-center justify-center">
+            <div className="text-xl font-extrabold text-slate-200">Zomato</div>
+            <div className="font-extrabold text-xs text-slate-200"> Mars, 12AM</div>
+          </div>
+          <div className="w-60 h-full items-center justify-center flex pt-6">
+            <div className="w-24 h-20 text-slate-200">눈내림</div>
+            <div className="w-28 h-20 text-3xl font-extrabold text-slate-200">20℃</div>
+          </div>
+        </div>
+        <div className="grid place-items-center rounded-full border-solid border-2 border-b-8 border-black w-16 h-16 content-center items-center m-auto ">
+          <div className=" font-normal text-5xl">
+           +
+          </div>
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+
+      <div className="flex-1 bg-slate-100 h-5/6 items-center justify-center">
+    <div className="grid place-items-center rounded-full border-solid border-2 border-b-4 border-black w-12 h-12 content-center items-center mt-8 ml-6">
+          <div className="font-normal text-3xl">
+           X
+          </div>
+        </div>
+        <div className="flex-col flex items-center justify-center mt-3 mb-5">
+          <div className="text-xl font-extrabold">SIMPLE TAG</div>
+          <div className="text-4xl font-extrabold">Work with the best</div>
+          <div className="text-4xl font-extrabold">designers</div>
+        </div>
+        <div className="flex flex-wrap h-2/3 items-center justify-center ml-5 mr-5">
+          <div className="rounded-lg w-40 h-56 border-2 border-black bg-yellow-400 mr-5">box</div>
+          <div className="rounded-lg w-40 h-56 border-2 border-black bg-cyan-600">box</div>
+          <div className="rounded-lg w-40 h-56 border-2 border-black bg-pink-400 mr-5 -mt-16">box</div>
+          <div className="rounded-lg w-40 h-56 border-2 border-black bg-orange-600 -mt-16">box</div>
+        </div>
+        <div className="flex rounded-lg bg-blue-700 text-2xl font-bold text-slate-100 items-center justify-center w-11/12 h-16 ml-5">Let's get it done</div>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+      <div className="flex-1 flex flex-col h-5/6 items-center justify-center bg-yellow-500">
+        <div className="flex h-32 w-11/12 items-center justify-center relative">
+          <div className="grid place-items-center rounded-full border-solid border-2 border-b-4 bg-slate-100 border-black w-14 h-14 absolute left-4"> Icon </div>
+          <div className="font-extrabold text-4xl">Friends</div>
+        </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="flex rounded-xl font-light text-xl border-solid border-2 border-b-8 border-black w-10/12 h-14 items-center pl-2 mb-8 bg-slate-100"> Search with love ... </div>
+
+        <div className="flex flex-col w-10/12 h-4/6 relative space-y-7">
+          <div className="flex rounded-xl border-solid border-2 border-black items-center space-y-2 h-20 bg-slate-100">
+          <div className="rounded-full border-solid border-2 border-black w-16 h-16 bg-orange-500 ml-4"></div>
+          <div className="flex flex-col ml-3">
+            <div className="font-extrabold text-2xl">Bill Rizer</div>
+            <div className="font-normal text-sm">planet Designer</div>
+          </div>
+          <div className="absolute right-7 border-solid border-2 border-b-4 border-black rounded-xl w-20 bg-yellow-500 font-extrabold text-lg h-10 pl-3 pt-1">Invite</div>
+          </div>
+          <div className="flex rounded-xl border-solid border-2 border-black items-center space-y-2 h-20 bg-slate-100">
+          <div className="rounded-full border-solid border-2 border-black w-16 h-16 bg-cyan-600 ml-4"></div>
+          <div className="flex flex-col ml-3">
+            <div className="font-extrabold text-2xl">Genbei Yagy</div>
+            <div className="font-normal text-sm">planet Designer</div>
+          </div>
+          <div className="absolute right-7 border-solid border-2 border-b-4 border-black rounded-xl w-20 bg-yellow-500 font-extrabold text-lg h-10 pl-3 pt-1">Invite</div>
+          </div>
+          <div className="flex rounded-xl border-solid border-2 border-black items-center space-y-2 h-20 bg-slate-100">
+          <div className="rounded-full border-solid border-2 border-black w-16 h-16 bg-orange-500 ml-4"></div>
+          <div className="flex flex-col ml-3">
+            <div className="font-extrabold text-2xl">Lancy Neo</div>
+            <div className="font-normal text-sm">Rogue Corp</div>
+          </div>
+          <div className="absolute right-7 border-solid border-2 border-b-4 border-black rounded-xl w-20 bg-yellow-500 font-extrabold text-lg h-10 pl-3 pt-1">Invite</div>
+          </div>
+          <div className="flex rounded-xl border-solid border-2 border-black items-center space-y-2 h-20 bg-slate-100">
+          <div className="rounded-full border-solid border-2 border-black w-16 h-16 bg-yellow-500 ml-4"></div>
+          <div className="flex flex-col ml-3">
+            <div className="font-extrabold text-2xl">Bill Rizer</div>
+            <div className="font-normal text-sm">Hard Cops</div>
+          </div>
+          <div className="absolute right-7 border-solid border-2 border-b-4 border-black rounded-xl w-20 bg-yellow-500 font-extrabold text-lg h-10 pl-3 pt-1">Invite</div>
+          </div>
+          <div className="flex rounded-xl border-solid border-2 border-black items-center space-y-2 h-20 bg-slate-100">
+          <div className="rounded-full border-solid border-2 border-black w-16 h-16 bg-pink-300 ml-4"></div>
+          <div className="flex flex-col ml-3">
+            <div className="font-extrabold text-2xl">Konami</div>
+            <div className="font-normal text-sm">xenan Createor</div>
+          </div>
+          <div className="absolute right-7 border-solid border-2 border-b-4 border-black rounded-xl w-20 bg-yellow-500 font-extrabold text-lg h-10 pl-3 pt-1">Invite</div>
+          </div>
+        </div>
       </div>
-    </main>
+    </div>
   );
 }
